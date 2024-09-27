4b4c5513c4af74f2c6e081ed0eb9bc346da58314
Rico Valentino <ricccv10@gmail.com>
2024-09-27 15:37:58 +0700

    Handson4

---
+++ ContohMethod.java
@@ -0,0 +1,12 @@
+public class ContohMethod {
+    public static int hitungLuasPersegi(int sisi) {
+        int luas =  sisi * sisi;
+        return luas;
+    }
+
+    public static void main(String[] args) {
+        int sisi= 5;
+        int luas = hitungLuasPersegi(sisi);
+        System.out.println("Luas persegi dengan sisi "+ sisi + " adalah " + luas);
+    }
+}
